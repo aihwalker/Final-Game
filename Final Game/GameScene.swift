10431d3e3aa020ccfd9aa69221c4021e3bc26486
student <student@awalker-2.local>
2018-04-11 11:43:31 -0500

    created app and added scrolling background

---
+++ Final Game/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  Final Game
+//
+//  Created by awalker on 4/10/18.
+//  Copyright © 2018 awalker. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+   
+    override func didMove(to view: SKView) {
+    createBackground()
+    }
+    
+    func createBackground(){
+        let waves = SKTexture(imageNamed:"waves")
+        for i in 0...1{
+            let wavesBackground = SKSpriteNode(texture: waves)
+            wavesBackground.zPosition = -1
+            wavesBackground.position = CGPoint(x: 0, y: wavesBackground.size.height * CGFloat(i))
+            addChild(wavesBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -wavesBackground.size.height, duration: 07)
+            let moveReset = SKAction.moveBy(x: 0, y: wavesBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            wavesBackground.run(moveForever)
+}
+}
+}
