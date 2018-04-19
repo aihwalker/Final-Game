4b0c6343db3fb400e62415b04833858c84844aae
student <student@awalker-2.local>
2018-04-19 11:40:36 -0500

    added boat

--- Final Game/GameScene.swift
+++ Final Game/GameScene.swift
@@ -5,14 +5,18 @@
 //  Created by awalker on 4/10/18.
 //  Copyright © 2018 awalker. All rights reserved.
 //
-// zimka test
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
    
+    var jetSki = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
-    createBackground()
+        
+        createBackground()
+        makeJetSki()
+    
     }
     
     func createBackground(){
@@ -22,11 +26,23 @@ class GameScene: SKScene {
             wavesBackground.zPosition = -1
             wavesBackground.position = CGPoint(x: 0, y: wavesBackground.size.height * CGFloat(i))
             addChild(wavesBackground)
-            let moveDown = SKAction.moveBy(x: 0, y: -wavesBackground.size.height, duration: 07)
+            let moveDown = SKAction.moveBy(x: 0, y: -wavesBackground.size.height, duration: 10)
             let moveReset = SKAction.moveBy(x: 0, y: wavesBackground.size.height, duration: 0)
             let moveLoop = SKAction.sequence([moveDown, moveReset])
             let moveForever = SKAction.repeatForever(moveLoop)
             wavesBackground.run(moveForever)
 }
+    }
+        
+        func makeJetSki() {
+            let jetSki = SKSpriteNode(imageNamed: "boat")
+            jetSki.position = CGPoint(x: frame.midX, y: frame.midY - 200)
+            jetSki.name = "jetSki"
+            jetSki.xScale = 0.5
+            jetSki.yScale = 0.5
+            jetSki.physicsBody = SKPhysicsBody(rectangleOf: jetSki.size)
+            jetSki.physicsBody?.isDynamic = false
+            addChild(jetSki)
 }
 }
+
